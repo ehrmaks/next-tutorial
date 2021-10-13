05b3e38657cdc3cd8784fcbed6d747cfda10b993
김진석 <jskim91@starlabs.co.kr>
2021-10-13 11:17:48 +0900

    deploy

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -5,15 +5,15 @@ DOCKER_APP_NAME=tutorial
 BLUE_FILE=${HOME_DIR}/deploy/docker-compose-blue.yml
 GREEN_FILE=${HOME_DIR}/deploy/docker-compose-green.yml
 
-EXIST_BLUE=$(sudo /usr/local/bin/docker-compose -p ${DOCKER_APP_NAME}-blue -f ${BLUE_FILE} ps | grep Up)
+EXIST_BLUE=$(sudo /usr/bin/docker-compose -p ${DOCKER_APP_NAME}-blue -f ${BLUE_FILE} ps | grep Up)
 
 if [ -z "$EXIST_BLUE" ]; then
     echo "blue up"
-    sudo /usr/local/bin/docker-compose -p ${DOCKER_APP_NAME}-blue -f ${BLUE_FILE} up -d --build
+    sudo /usr/bin/docker-compose -p ${DOCKER_APP_NAME}-blue -f ${BLUE_FILE} up -d --build
     sleep 5
     # RUNNING=$(docker inspect --format="{{.State.Running}}" ${DOCKER_APP_NAME}-blue_nextjs_1 2> /dev/null)
     # if [ "$RUNNING" == "true" ]; then
-    sudo /usr/local/bin/docker-compose -p ${DOCKER_APP_NAME}-green -f ${GREEN_FILE} down
+    sudo /usr/bin/docker-compose -p ${DOCKER_APP_NAME}-green -f ${GREEN_FILE} down
     sudo /usr/bin/docker system prune -f
     #     exit 0
     # else
@@ -22,11 +22,11 @@ if [ -z "$EXIST_BLUE" ]; then
     # fi
 else
     echo "green up"
-    sudo /usr/local/bin/docker-compose -p ${DOCKER_APP_NAME}-green -f ${GREEN_FILE} up -d --build
+    sudo /usr/bin/docker-compose -p ${DOCKER_APP_NAME}-green -f ${GREEN_FILE} up -d --build
     sleep 5
     # RUNNING=$(docker inspect --format="{{.State.Running}}" ${DOCKER_APP_NAME}-green_nextjs_1 2> /dev/null)
     # if [ "$RUNNING" == "true" ]; then
-    sudo /usr/local/bin/docker-compose -p ${DOCKER_APP_NAME}-blue -f ${BLUE_FILE} down
+    sudo /usr/bin/docker-compose -p ${DOCKER_APP_NAME}-blue -f ${BLUE_FILE} down
     sudo /usr/bin/docker system prune -f
     #     exit 0
     # else
