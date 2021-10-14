51f087f4eab2612b6b09559f6e4de2a86d51edb9
김진석 <jskim91@starlabs.co.kr>
2021-10-14 13:09:27 +0900

    ff

--- pages/index.tsx
+++ pages/index.tsx
@@ -53,12 +53,12 @@ export default function Home({ list }: ListType) {
 			{/* {!isLoading && ( */}
 			<>
 				<Header as="h3" style={{ paddingTop: 20 }}>
-					Best 상품
+					Best 상품f
 				</Header>
 				<Divider></Divider>
 				<ItemList list={list.slice(0, 9)}></ItemList>
 				<Header as="h3" style={{ paddingTop: 20 }}>
-					New 상품
+					New 상품f
 				</Header>
 				<Divider></Divider>
 				<ItemList list={list.slice(9)}></ItemList>
