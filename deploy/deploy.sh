c7140017f2a18ecc97011fdf7bf0048693e0f226
김진석 <jskim91@starlabs.co.kr>
2021-10-14 12:46:24 +0900

    gogo

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -13,11 +13,10 @@ if [ -z "$EXIST_BLUE" ]; then
     sleep 5
     # RUNNING=$(docker inspect --format="{{.State.Running}}" ${DOCKER_APP_NAME}-blue_nextjs_1 2> /dev/null)
     # if [ "$RUNNING" == "true" ]; then
-    sudo /usr/bin/docker-compose -p ${DOCKER_APP_NAME}-green -f ${GREEN_FILE} stop
+    sudo /usr/bin/docker-compose -p ${DOCKER_APP_NAME}-green -f ${GREEN_FILE} down
+    sudo docker rm -f $(docker ps -aq)
     sudo /usr/bin/docker system prune -f
 
-    sudo systemctl start docker
-    sudo chmod 666 /var/run/docker.sock
     #     exit 0
     # else
     #     /usr/bin/docker system prune -f
@@ -29,11 +28,10 @@ else
     sleep 5
     # RUNNING=$(docker inspect --format="{{.State.Running}}" ${DOCKER_APP_NAME}-green_nextjs_1 2> /dev/null)
     # if [ "$RUNNING" == "true" ]; then
-    sudo /usr/bin/docker-compose -p ${DOCKER_APP_NAME}-blue -f ${BLUE_FILE} stop
+    sudo /usr/bin/docker-compose -p ${DOCKER_APP_NAME}-blue -f ${BLUE_FILE} down
+    sudo docker rm -f $(docker ps -aq)
     sudo /usr/bin/docker system prune -f
 
-    sudo systemctl start docker
-    sudo chmod 666 /var/run/docker.sock
     #     exit 0
     # else
     #     /usr/bin/docker system prune -f
