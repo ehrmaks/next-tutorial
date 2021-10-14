f181ea7b511958b9a17e0cf604a888d37383120f
김진석 <jskim91@starlabs.co.kr>
2021-10-14 18:05:53 +0900

    c

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -11,35 +11,17 @@ if [ -z "$EXIST_BLUE" ]; then
     echo "blue up"
     sudo /usr/bin/docker-compose -p ${DOCKER_APP_NAME}-blue -f ${BLUE_FILE} up -d --build
     sleep 5
-    # RUNNING=$(docker inspect --format="{{.State.Running}}" ${DOCKER_APP_NAME}-blue_nextjs_1 2> /dev/null)
-    # if [ "$RUNNING" == "true" ]; then
     sudo /usr/bin/docker-compose -p ${DOCKER_APP_NAME}-green -f ${GREEN_FILE} down
     echo "docker object remove..."
     sudo /usr/bin/docker system prune -f
     echo "docker object remove complete."
-
-    
-
-    #     exit 0
-    # else
-    #     /usr/bin/docker system prune -f
-    #     exit 0
-    # fi
 else
     echo "green up"
     sudo /usr/bin/docker-compose -p ${DOCKER_APP_NAME}-green -f ${GREEN_FILE} up -d --build
     sleep 5
-    # RUNNING=$(docker inspect --format="{{.State.Running}}" ${DOCKER_APP_NAME}-green_nextjs_1 2> /dev/null)
-    # if [ "$RUNNING" == "true" ]; then
     sudo /usr/bin/docker-compose -p ${DOCKER_APP_NAME}-blue -f ${BLUE_FILE} down
     echo "docker object remove..."
     sudo /usr/bin/docker system prune -f
     echo "docker object remove complete."
-
-    #     exit 0
-    # else
-    #     /usr/bin/docker system prune -f
-    #     exit 0
-    # fi
 fi
 exit 0
