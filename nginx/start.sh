a16145c485dec790961265373c8203e10292c9d4
김진석 <jskim91@starlabs.co.kr>
2021-10-15 10:16:14 +0900

    nginx

---
+++ nginx/start.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+HOME_DIR=/home/ec2-user/next-tutorial
+DOCKER_APP_NAME=tutorial
+NGINX_FILE=${HOME_DIR}/nginx/docker-compose-nginx.yml
+
+echo "nginx down"
+sudo /usr/bin/docker-compose -p ${DOCKER_APP_NAME}-nginx -f ${NGINX_FILE} down
+
+echo "nginx up"
+sudo /usr/bin/docker-compose -p ${DOCKER_APP_NAME}-nginx -f ${NGINX_FILE} up -d --build
+sleep 5
+sudo /usr/bin/docker system prune -f
+
+exit 0
